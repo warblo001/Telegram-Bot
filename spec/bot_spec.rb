85425d0a1ba3b776dc1554b2432175e549d610c5
warblo001 <67009988+warblo001@users.noreply.github.com>
2021-01-21 12:01:06 +0100

    Add more rspec tests.
    Add more positive and negative rspec tests.

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -11,3 +11,20 @@ describe Bot do
     expect(@play_link).not_to eql(['Top 500 classic Rock songs', 'Modern Rock Hits', 'Late 90s/Early 2000s Rock Playlist'])
   end
 end
+
+describe Choices do
+  it 'Returns the playlist of the choice' do
+    thebot = Bot.new
+    thebot.extend(Choices)
+    expect(thebot.choice('Top 500 classic Rock songs',
+                         ['Top 500 classic Rock songs', 'Alternative rock of the 2000s'])).to eql('Top 500 classic Rock songs')
+  end
+end
+
+describe Choices do
+  it 'Will return Nil if the selection does not match' do
+    thebot = Bot.new
+    thebot.extend(Choices)
+    expect(thebot.choice('top 500', ['Top 500 classic Rock songs', 'Alternative rock of the 2000s'])).to eql(nil)
+  end
+end
